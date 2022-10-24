04f976a8c8df56fadbfddd987acac7798fad0369
Oliver Schulz <oliver.schulz@student.hpi.de>
2022-10-24 16:51:00 +0200

    Failing test for #3

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe "Author Model", type: :model do
+    author = Author.new(first_name: "Max", last_name: "Mustermann", homepage: "maxmustermann.de")
+    it "should have a first name, last name and homepage" do
+        expect(author.first_name).to be_a(String)
+        expect(author.last_name).to be_a(String)
+        expect(author.homepage).to be_a(String)
+    end
+    it "should have a name method that returns the full name" do
+        expect(author.name).to eq("Max Mustermann")
+    end
+end
