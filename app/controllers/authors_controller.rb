c53970fc5ecfcd3a0437138d9ad73631dd068a05
Oliver Schulz <oliver.schulz@student.hpi.de>
2022-10-25 17:43:55 +0200

    #4

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,7 +5,21 @@ class AuthorsController < ApplicationController
   def new
     @author = Author.new
   end
+  def create
+    @author = Author.new(author_params)
+
+    if @author.save
+      redirect_to root_path, notice: 'Success!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   def show
   end
+
+  private
+    def author_params
+      params.require(:author).permit(:first_name, :last_name, :homepage)
+    end
 end
