32d79d6c59ee6610e69f2f200ae57bdce7f9a7be
Oliver Schulz <oliver.schulz@student.hpi.de>
2022-10-26 14:51:33 +0200

    Fix #18

---
+++ spec/features/papers/edit_paper_spec.rb
@@ -0,0 +1,10 @@
+# require 'rails_helper'
+
+# describe "Edit paper page", type: :feature do
+#   it "should be able to select multiple authors" do
+#     # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
+#     @paper = FactoryBot.create(:paper)
+#     visit paper_path(@paper)
+#     expect(page).to have_css("select[multiple]")
+#   end
+# end
