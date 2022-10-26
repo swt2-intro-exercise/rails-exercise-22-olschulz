5062926db4eacf58c18b6138fdb3294ccd55d998
Oliver Schulz <oliver.schulz@student.hpi.de>
2022-10-26 14:14:34 +0200

    #16

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,5 +1,6 @@
 class Paper < ApplicationRecord
-        validates :title, presence: true
-        validates :venue, presence: true
-        validates :year, presence: true, numericality: { only_integer: true }
+    has_and_belongs_to_many :authors
+    validates :title, presence: true
+    validates :venue, presence: true
+    validates :year, presence: true, numericality: { only_integer: true }
 end
