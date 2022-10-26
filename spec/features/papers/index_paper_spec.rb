e98b542d364a2f9b27635fc6c73adedeb5851c65
Oliver Schulz <oliver.schulz@student.hpi.de>
2022-10-26 14:04:13 +0200

    Failing test for #15

--- spec/features/papers/index_paper_spec.rb
+++ spec/features/papers/index_paper_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 describe "index paper page", type: :feature do
     it "should link to each paper's edit page" do
-    @paper = FactoryBot.create(:paper)
-    visit papers_path
-    expect(page).to have_link 'Edit', href: edit_paper_path(@paper)
+        @paper = FactoryBot.create(:paper)
+        visit papers_path
+        expect(page).to have_link 'Edit', href: edit_paper_path(@paper)
+    end
+    it "should be possible to delete a paper with a link" do
+        @paper = FactoryBot.create(:paper)
+        visit papers_path
+        expect(page).to have_link 'Delete', href: paper_path(@paper)
     end
 end
